18cd31f4ba96d764d339ea11927ba7092c97603e
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-07-28 13:28:28 +0200

    Numbers>6, Numbers*2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     return (
         <div>
             <Numbers numbers={NUMBER_DATA} title="Numbers"/>
+            <Numbers numbers={NUMBER_DATA.filter(n => n > 6)} title="Numbers>6"/>
+            <Numbers numbers={NUMBER_DATA.map(n => n * 2)} title="Numbers*2"/>
         </div>
     );
 }
