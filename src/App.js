9d71456451ae8c50967d45232d5a56f94235fba8
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-07-28 16:43:06 +0200

    persons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import {CARS, NUMBER_DATA, PERSON_DATA} from "./ data/data";
 import {Numbers} from "./components/Numbers";
 import {Cars} from "./components/Cars";
-import {Person} from "./components/Person";
+import {Persons} from "./components/Person";
 
 function App() {
     return (
@@ -11,7 +11,7 @@ function App() {
             <Numbers numbers={NUMBER_DATA.filter(n => n > 6)} title="Numbers>6"/>
             <Numbers numbers={NUMBER_DATA.map(n => n * 2)} title="Numbers*2"/>
             <Cars cars={CARS} title="Auto's"/>
-            <Person person={PERSON_DATA[0]}/>
+            <Persons persons={PERSON_DATA} title="Personen"/>
         </div>
     );
 }
--- src/components/Person.js
+++ src/components/Person.js
@@ -4,6 +4,13 @@ export function Person(props) {
         <h4>{person.name}</h4>
         <div>{person.age}</div>
         <div>{person.city}</div>
-    </div>
+    </div>;
 }
 
+export function Persons(props) {
+    const {persons, title} = props;
+    return <div className="section">
+        <h3>{title}</h3>
+        {persons.map(p => <Person key={p.id} person={p}/>)}
+    </div>;
+}
