e65380ec268182753acf0b05589a243b83bd908f
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-07-29 20:10:09 +0200

    EventDemo with stopPropagation

--- src/components/EventDemo.js
+++ src/components/EventDemo.js
@@ -2,12 +2,24 @@ import {Section} from "./Section";
 
 export function EventDemo() {
     return <Section title="events">
-        <div style={{backgroundColor: "pink"}}>
+        <div style={{backgroundColor: "pink"}}
+             onClick={() => {
+                 alert("div is clicked");
+             }}>
             <a href="http://google.com"
+               onClick={(e) => {
+                   e.preventDefault();
+                   e.stopPropagation();
+                   alert("link is clicked");
+               }}>dit is een link met een href</a>
+            &nbsp;- en -&nbsp;
+            <button
                 onClick={(e) => {
-                    e.preventDefault();
-                    alert("link is clicked");
-                }}>dit is een link met een href</a>
+                    e.stopPropagation();
+                    alert("button is clicked");
+                }}>
+                dit is een button
+            </button>
         </div>
     </Section>;
 }
