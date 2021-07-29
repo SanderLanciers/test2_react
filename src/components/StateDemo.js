e0ab73427783162d8aa1696c89f19ed8e0b8a1a3
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-07-29 20:38:14 +0200

    StateDemo with state counter

--- src/components/StateDemo.js
+++ src/components/StateDemo.js
@@ -3,6 +3,7 @@ import {Section} from "./Section";
 
 export function StateDemo() {
     const [isOn, setIsOn] = useState(false);
+    const [counter, setCounter] = useState(0);
 
     return <Section title="state">
         <h3>huidige waarde van state is: {isOn ? "ON" : "OFF"}</h3>
@@ -10,6 +11,15 @@ export function StateDemo() {
         <button onClick={() => setIsOn(false)}>verander de state in false</button>
         <button onClick={() => setIsOn(!isOn)}>toggle state</button>
         <div>huidige waarde van state is: {isOn ? "ON" : "OFF"}</div>
+        <hr/>
+        <div>counter is {counter}</div>
+        <button onClick={() => setCounter(counter+1)}>+</button>
+        <button onClick={() => setCounter(counter-1)}>-</button>
+        <hr/>
+        <div>counter is {counter}</div>
+        <button onClick={() => setCounter(counter+1)}>+</button>
+        <button onClick={() => setCounter(counter-1)}>-</button>
+        <hr/>
     </Section>;
 }
 
