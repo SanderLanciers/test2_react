b828e75035f34ebab3f0f051a285a0e10eefad6e
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-07-30 14:06:19 +0200

    browse through Persons

--- src/components/PersonBrowser.js
+++ src/components/PersonBrowser.js
@@ -6,8 +6,28 @@ export function PersonBrowser(props) {
     const {persons} = props;
     const [shownPerson, setShownPerson] = useState(persons[0]);
 
+    function setNextShownPerson() {
+        const indexOfShownPerson = persons.findIndex(p => p.id === shownPerson.id);
+        const indexOfNextPerson = indexOfShownPerson < persons.length - 1 ? indexOfShownPerson + 1 : 0;
+        setShownPerson(persons[indexOfNextPerson]);
+    }
+
+    function setPrevShownPerson() {
+        const indexOfShownPerson = persons.findIndex(p => p.id === shownPerson.id);
+        const indexOfNextPerson = indexOfShownPerson === 0 ? persons.length - 1 : indexOfShownPerson - 1;
+        setShownPerson(persons[indexOfNextPerson]);
+    }
+
     return <Section title="Mijn Favoriete Personen">
-        <Person person={shownPerson}/>
+        <div className="personWithButtons">
+            <div>
+                <button onClick={() => setPrevShownPerson()}>&lt;</button>
+            </div>
+            <Person person={shownPerson}/>
+            <div>
+                <button onClick={() => setNextShownPerson()}>&gt;</button>
+            </div>
+        </div>
     </Section>;
 }
 
