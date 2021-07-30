b9c836ce51e37ca2646646e8b5e0b953e5410e5b
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-07-30 14:39:02 +0200

    open/close Section Component

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,13 +1,15 @@
+import {useState} from "react";
+
 export function Section(props) {
     const {children, title} = props;
+    const [isOpen, setIsOpen] = useState(false);
 
     return <div className="section">
-        <button
-            onClick={(e) => alert(`hallo (${e.clientX}, ${e.clientY})`)}>
-            klik
-        </button>
         <h3>{title}</h3>
-        {children}
+        <div>
+            <button onClick={() => setIsOpen(!isOpen)}>{isOpen ? "-" : "+"}</button>
+        </div>
+        {isOpen && children}
     </div>;
 }
 
