fd9992effd128ea9926595f9d192805dc5b78607
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-07-30 09:09:17 +0200

    toggle favoriteNumbers

--- src/components/StateDemo.js
+++ src/components/StateDemo.js
@@ -6,7 +6,7 @@ export function StateDemo() {
     const [isOn, setIsOn] = useState(false);
     const [counter, setCounter] = useState(0);
     const [favoriteNumbers, setFavoriteNumbers] = useState([]);
-
+    const isCounterFavorite = favoriteNumbers.includes(counter);
     return <Section title="state">
         <h3>huidige waarde van state is: {isOn ? "ON" : "OFF"}</h3>
         <button onClick={() => setIsOn(true)}>verander de state in true</button>
@@ -23,8 +23,15 @@ export function StateDemo() {
         <button onClick={() => setCounter(counter - 1)}>-</button>
         <hr/>
         <Numbers numbers={favoriteNumbers} title="favoriete getallen"/>
-        <button onClick={() => setFavoriteNumbers([...favoriteNumbers, counter])}>
-            kies {counter} als favoriet
+        <button
+            onClick={() => {
+                if (favoriteNumbers.includes(counter))
+                    setFavoriteNumbers(favoriteNumbers.filter((n) => n !== counter));
+                else
+                    setFavoriteNumbers([...favoriteNumbers, counter]);
+            }}>
+            {isCounterFavorite && `verwijder ${counter} als favoriet`}
+            {!isCounterFavorite && `kies ${counter} als favoriet`}
         </button>
     </Section>;
 }
