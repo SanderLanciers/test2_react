721f463725c91dd1467d7d61241081032458a11f
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-10-11 12:11:24 +0200

    start test 2

--- src/App.js
+++ src/App.js
@@ -11,23 +11,8 @@ import {PersonBrowser} from "./components/PersonBrowser";
 
 function App() {
     return (
-        <div>
-            <Numbers numbers={NUMBER_DATA} title="Numbers"/>
-            <Numbers numbers={NUMBER_DATA.filter(n => n > 6)} title="Numbers>6"/>
-            <Numbers numbers={NUMBER_DATA.map(n => n * 2)} title="Numbers*2"/>
-            <Cars cars={CARS} title="Auto's"/>
-            <Persons persons={PERSON_DATA} title="Personen"/>
-            <Persons persons={[...PERSON_DATA].sort((pl, pr) => pl.age - pr.age)} title="Personen volgens leeftijd"/>
-            <Numbers numbers={PERSON_DATA.map(p => p.age)} title="Leeftijden van de Personen"/>
-            <Numbers numbers={[...new Set(PERSON_DATA.map(p => p.age).sort((al, ar) => al - ar))]}
-                     title="Leeftijden van de Personen gesorteerd"/>
-            <Cities cities={[{name: "Antwerpen", numberOfPersons: 111}, {name: "Brussel", numberOfPersons: 32}]}
-                    title="City test"/>
-            <Cities cities={citiesFromPersonData(PERSON_DATA)} title="Cities"/>
-            <EventDemo/>
-            <StateDemo/>
-            <PersonBrowser persons={PERSON_DATA}/>
-        </div>
+        <>
+        </>
     );
 }
 
