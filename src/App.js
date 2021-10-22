a15729a755b3433aad3a5679092caa342c8a32fc
Sander Lanciers <r0850980@student.thomasmore.be>
2021-10-22 11:07:42 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
-import {CARS, NUMBER_DATA, PERSON_DATA} from "./ data/data";
-import {Numbers} from "./components/Numbers";
-import {Cars} from "./components/Cars";
-import {Persons} from "./components/Person";
-import {Cities} from "./components/City";
-import {citiesFromPersonData} from "./utilities/person_utilities";
-import {EventDemo} from "./components/EventDemo";
-import {StateDemo} from "./components/StateDemo";
-import {PersonBrowser} from "./components/PersonBrowser";
+import {HOUSE_DATA} from "./ data/data";
+import {Houses} from "./components/Houses";
+
 
 function App() {
     return (
         <>
+            <Houses houses={HOUSE_DATA} title={"Luxary Houses for sale"}></Houses>
         </>
     );
 }
